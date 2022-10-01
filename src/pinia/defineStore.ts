0e31ed977a5f1bb2a3be9d23932e774f808883f9
dduohereshui <2245675739@qq.com>
2022-10-01 18:47:59 +0800

    setupStore 状态设置

--- src/pinia/defineStore.ts
+++ src/pinia/defineStore.ts
@@ -4,6 +4,8 @@ import {
   inject,
   reactive,
   computed,
+  isRef,
+  isReactive,
 } from "vue";
 import type { EffectScope } from "vue";
 import { Pinia, piniaSymbol } from "./rootStore";
@@ -12,6 +14,9 @@ type WithIdOptions = {
   id: string;
   [key: string]: any;
 };
+function isComputed(v: any) {
+  return !!(isRef(v) && v.effect);
+}
 
 export function defineStore(idOrOptions: string | WithIdOptions, setup: any) {
   let id: string;
@@ -33,7 +38,7 @@ export function defineStore(idOrOptions: string | WithIdOptions, setup: any) {
     if (!pinia?._s.has(id)) {
       // 第一次useStore
       if (isSetupStore) {
-        createSetupStore(id, setup, pinia);
+        createSetupStore(id, setup, pinia!, false);
       } else {
         createOptionStore(id, options, pinia!);
       }
@@ -69,14 +74,23 @@ function createOptionStore(
     );
     return loaclState;
   }
-
-  return createSetupStore(id, setup, pinia);
+  // isOption
+  return createSetupStore(id, setup, pinia, true);
 }
 
-function createSetupStore(id: string, setup: () => any, pinia: Pinia) {
+function createSetupStore(
+  id: string,
+  setup: () => any,
+  pinia: Pinia,
+  isOption: boolean
+) {
   let scope: EffectScope;
   const store = reactive<any>({});
-
+  // 对于setupStore，initial目前不存在
+  const initialState = pinia.state.value[id];
+  if (!initialState && !isOption) {
+    pinia.state.value[id] = {};
+  }
   const setupStore = pinia._e.run(() => {
     scope = effectScope();
     // 父亲能停止所有，自己也能停止自己
@@ -98,10 +112,21 @@ function createSetupStore(id: string, setup: () => any, pinia: Pinia) {
     if (typeof prop === "function") {
       setupStore[key] = wrapAction(key, prop);
     }
+
+    // 判断该值是否为状态
+    if ((isRef(prop) && !isComputed(prop)) || isReactive(prop)) {
+      if (!isOption) {
+        // console.log("prop: ", id, key, prop);
+        pinia.state.value[id][key] = prop;
+      }
+    }
   }
 
   Object.assign(store, setupStore);
 
   pinia._s.set(id, store);
+
+  console.log(pinia);
+
   return store;
 }
